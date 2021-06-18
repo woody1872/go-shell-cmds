ac03d1f6cb5053ab4296620358b2bb7b039ed126
Sam Wood <samwooddev@gmail.com>
2021-06-18 23:19:51 +0100

    Initial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SWOOD1872/go-shell-cmds
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"log"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	var ip string
+	flag.StringVar(&ip, "i", "8.8.8.8", "ip address")
+
+	cmd := exec.Command("ping", "-c 2", "-i 1", ip)
+
+	var buf bytes.Buffer
+	cmd.Stdout = &buf
+
+	if err := cmd.Start(); err != nil {
+		log.Fatalln(err)
+	}
+
+	done := make(chan error)
+	go func() { done <- cmd.Wait() }()
+
+	timeout := time.After(5 * time.Second)
+
+	select {
+	case <-timeout:
+		cmd.Process.Kill()
+		log.Fatalln("ERROR: command timed out")
+	case err := <-done:
+		if err != nil {
+			log.Fatalln("ERROR:", err)
+		}
+	}
+}
